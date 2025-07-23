66aea527f033eebfdef442c9ec67c8f2086b6b24
quia0 <tanyakia00@gmail.com>
2025-07-23 01:37:55 -0700

    si Sir. Bus ay pogi hehe

---
+++ src/tanyakia/Tanyakia.java
@@ -0,0 +1,23 @@
+
+package tanyakia;
+
+import java.util.Scanner
+
+public class Tanyakia {
+
+   
+    public static void main(String[] args) {
+        Scanner ki = new Scanner (System.in);
+        String nm;
+        int byear;
+        
+        System.out.print("Enter your name: ");
+        nm = ki.nextLine();
+        
+        System.out.print("Enter your birth year: ");
+        byear = ki.nextInt();
+        
+        System.out.print("\nHello " + nm + "your age is" +(2025-byear));
+    }
+    
+}
