ecfe57b06b22fd8c94b2dbc5a4f3ed46d1bdb5af
quia0 <tanyakia00@gmail.com>
2025-07-23 01:55:32 -0700

    i uploaded activity 2

---
+++ src/tanyakia/Activity2.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tanyakia;
+
+/**
+ *
+ * @author PC 33
+ */
+public class Activity2 {
+    
+}
